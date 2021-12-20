aae9be59a2291b6facb8a1f4df3d08689eb651ae
julio_castillo <julio_castillo19922005@outlook.com>
2021-12-20 06:36:49 -0600

    Curso Angular 15 Directicas II

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,17 @@ export class AppComponent {
   registrado: boolean = false;
   nombre:string = "";
   apellido:string = "";
+  entradas: any;
+
+  constructor(){
+    this.entradas = [
+      {titulo:"Pyhton cada día más presente"},
+      {titulo:"Java presente desde hace más de 20 años"},
+      {titulo:"JavaScript cada vez más funcional"},
+      {titulo:"Kotlin potencia para tus apps"},
+      {titulo:"¿Dónde quedo pascal?"},
+    ]
+  }
   registrarUsuario(){
     this.registrado = true;
     this.mensaje = "Usuario registrado con exito con el nombre de: ";
